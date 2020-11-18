2c95759d3dfffd538f71020ca8724cb3eec297f3
czulueta <https://github.com/czulueta/weed.git>
2020-11-18 00:09:06 -0700

    form setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function App() {
+  return (
+    <div>
+      <form>
+        <input>FirstName</input>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
